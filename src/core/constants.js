1084068d5846bc967f5196bea1eeb5f6542aa0e0
Puhiza Aliu <aliupuhiza@gmail.com>
2026-04-15 14:18:43 +0200

    create player constants and lane positions

---
+++ src/core/constants.js
@@ -0,0 +1,18 @@
+export const LANE_POSITIONS = [-1.8, 0, 1.8];
+
+export const PLAYER_DEFAULTS = {
+  lane: 0,
+  x: 0,
+  y: 0.2,
+  z: 3,
+  width: 0.9,
+  height: 1.4,
+  depth: 0.9,
+  color: 0xb02a1d,
+  isJumping: false,
+  jumpVelocity: 0,
+  gravity: 0.010,
+  laneLerpFactor: 0.18,
+  jumpStrength: 0.16,
+  groundY: 0.2,
+};
