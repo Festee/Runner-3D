369c25182048071c7c021e68d96acdf54447d5cf
Festee <fd61273@ubt-uni.net>
2026-04-15 19:31:01 +0200

    Extract world textures into separate module

---
+++ src/render/textures.js
@@ -0,0 +1,35 @@
+import * as THREE from 'https://unpkg.com/three@0.183.2/build/three.module.js';
+
+export function loadWorldTextures() {
+  const loader = new THREE.TextureLoader();
+
+  const groundTexture = loader.load(
+    './src/assets/Ground.png',
+    (tex) => {
+      tex.wrapS = THREE.RepeatWrapping;
+      tex.wrapT = THREE.RepeatWrapping;
+      tex.repeat.set(1, 12);
+      tex.colorSpace = THREE.SRGBColorSpace;
+    },
+    undefined,
+    () => console.warn('Ground.png not found')
+  );
+
+  const wallTexture = loader.load(
+    './src/assets/Wall.png',
+    (tex) => {
+      tex.wrapS = THREE.RepeatWrapping;
+      tex.wrapT = THREE.RepeatWrapping;
+      tex.repeat.set(1, 20);
+      tex.offset.y = 0;
+      tex.colorSpace = THREE.SRGBColorSpace;
+    },
+    undefined,
+    () => console.warn('Wall.png not found')
+  );
+
+  return {
+    groundTexture,
+    wallTexture,
+  };
+}
