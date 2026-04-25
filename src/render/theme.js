b6ef0c48817a7dc411ba479c3619b2084c814f9b
Festee <fd61273@ubt-uni.net>
2026-04-26 00:24:39 +0200

    Theme changes for basic polish

--- src/render/theme.js
+++ src/render/theme.js
@@ -3,6 +3,8 @@ import * as THREE from 'https://unpkg.com/three@0.183.2/build/three.module.js';
 function applyLampState(world, isNightMode) {
   const lampIntensity = isNightMode ? 1.5 : 0.0;
   const emissiveIntensity = isNightMode ? 0.85 : 0.0;
+  const markerEmissiveIntensity = isNightMode ? 0.2 : 0.0;
+  const edgeEmissiveIntensity = isNightMode ? 0.3 : 0.0;
 
   world.segmentMeshes.forEach((segmentMeshes) => {
     const lamps = [segmentMeshes.leftLamp, segmentMeshes.rightLamp];
@@ -11,6 +13,12 @@ function applyLampState(world, isNightMode) {
       lamp.light.intensity = lampIntensity;
       lamp.bulb.material.emissiveIntensity = emissiveIntensity;
     });
+
+    segmentMeshes.laneMarkers.forEach((marker) => {
+      marker.material.emissiveIntensity = markerEmissiveIntensity;
+    });
+    segmentMeshes.leftEdgeStrip.material.emissiveIntensity = edgeEmissiveIntensity;
+    segmentMeshes.rightEdgeStrip.material.emissiveIntensity = edgeEmissiveIntensity;
   });
 }
 
@@ -20,15 +28,15 @@ export function applyVisualMode(scene, world, lights, visualMode) {
   if (isNightMode) {
     scene.background = new THREE.Color(0x0b1021);
     scene.fog = new THREE.Fog(0x0b1021, 10, 65);
-    lights.ambientLight.intensity = 0.2;
-    lights.directionalLight.intensity = 0.35;
+    lights.ambientLight.intensity = 0.24;
+    lights.directionalLight.intensity = 0.4;
     lights.directionalLight.color.setHex(0xa9bbff);
-    lights.nightFillLight.intensity = 0.7;
+    lights.nightFillLight.intensity = 0.75;
   } else {
     scene.background = new THREE.Color(0x87ceeb);
     scene.fog = new THREE.Fog(0x87ceeb, 18, 85);
-    lights.ambientLight.intensity = 0.9;
-    lights.directionalLight.intensity = 1.3;
+    lights.ambientLight.intensity = 0.95;
+    lights.directionalLight.intensity = 1.35;
     lights.directionalLight.color.setHex(0xffffff);
     lights.nightFillLight.intensity = 0.0;
   }
